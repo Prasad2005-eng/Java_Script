f340b21de43b02119f98f4a8e611530b10fdd6a8
INGLE PRASAD MURLIDHAR <ingleprasad2005@gmail.com>
2026-06-23 08:49:04 +0000

    imp conversion and operations

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,21 @@
+ let score = "33abc" 
+ console.log(typeof(score));
+
+ let value = Number(score)
+ console.log(typeof (value))
+ console.log(value)  
+
+ // Operations //
+
+//  let value1 = 3
+//  let negvalue = -value1
+//  console.log(negvalue)
+//  console.log(2/3)
+//  console.log(2%3)
+
+let str1 = "Hello"
+let str2 = " Prasad"
+let str3 = str1 + str2
+console.log(str3)
+console.log("1" + 2+2)
+console.log(1+2+"2")  
