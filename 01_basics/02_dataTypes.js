0192b1f9ec783dcad231d706ea1cec275c9a71d5
INGLE PRASAD MURLIDHAR <ingleprasad2005@gmail.com>
2026-06-23 08:20:55 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+  "use strict" //trat all JS code as a newer version
+
+ //  alert(3 + 2) we are using nodejs not browser
+ 
+ let name = "Prasad" //string
+ let age = 21 //Number - 2 to the power 53
+ let isloogedIn = false // boolean 
+
+
+//extra dataTypes
+ //bigint 
+ //null =>standalone value
+ //undefined 
+ //symbol => unique
+  
+ //object -Imp
+
+ console.log(typeof "Hitesh");
+ console.log(typeof null);
+ console.log(typeof undefined);
