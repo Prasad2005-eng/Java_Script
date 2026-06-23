426e94fa1fa7a741b57bfdf4a4d7382419aaebb9
INGLE PRASAD MURLIDHAR <ingleprasad2005@gmail.com>
2026-06-23 08:04:46 +0000

    some Important about variable in JS

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,23 @@
+  const accountId = 122232
+  let email = "Prasad@gmail.com"
+  var password = "5555"
+  accountCity = "Mumbai"
+  let accountState;
+
+   email = "hp@google.com"
+  password = "11111111"
+  accountCity = "Pune"
+
+//   accountId = 2 //not allowed
+  console.log(accountId);  
+  /*
+  Prefer not to use v ar beacuse of issue in block scope and functional scope. 
+  */
+
+   console.table([accountId,email,password,accountCity,accountState])
+
+
+ 
+
+
+    
