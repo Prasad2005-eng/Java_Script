b77ccec1fa3fb8c66e4cd792e0ec6f20cfe7ce00
INGLE PRASAD MURLIDHAR <ingleprasad2005@gmail.com>
2026-06-24 08:24:50 +0000

    Date time baisc

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,10 @@
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toJSON())
+
+let myCreateDate = new Date(2023,0,24,6,6)
+console.log(myCreateDate.toLocaleDateString());
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
