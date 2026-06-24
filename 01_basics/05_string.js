bc1edbbe5b809447db4bcd3dc309c86e325b21d7
INGLE PRASAD MURLIDHAR <ingleprasad2005@gmail.com>
2026-06-24 07:48:02 +0000

    string basic

---
+++ 01_basics/05_string.js
@@ -0,0 +1,9 @@
+const name = "Prasad"
+const age = 21;
+console.log(name + age); 
+
+console.log(`Hello my name is ${name} and my age is ${age}`);
+
+const gameName = new String('Prasad 2');
+console.log(gameName[0]);
+console.log(gameName.toLocaleUpperCase);
